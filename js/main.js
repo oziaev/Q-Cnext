b8c5d39e388f3af329e6552cbc0980277eea49f7
yuoz <oz12iaev@gmail.com>
2026-04-07 01:27:05 +0900

    初回アップ

---
+++ js/main.js
@@ -0,0 +1,74 @@
+$(function () {
+  // --------------------------------
+  // ヘッダー
+  // --------------------------------
+  const header = document.querySelector("header");
+  const toggle = document.querySelector(".toggle");
+
+  toggle.addEventListener("click", (e) => {
+    e.stopPropagation();
+    header.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
+  });
+
+  const closeMenu = () => {
+    header.classList.remove("active");
+    document.body.classList.remove("no-scroll");
+  };
+
+  document.addEventListener("click", () => {
+    if (header.classList.contains("active")) {
+      closeMenu();
+    }
+  });
+
+  // --------------------------------
+  // スライダー
+  // --------------------------------
+  $(".slide-items").slick({
+    centerMode: true,
+    centerPadding: "10%",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  // --------------------------------
+  // モーダル
+  // --------------------------------
+  // $(".js-modal-open").on("click", function (e) {
+  //   e.preventDefault();
+  //   $("#modal-water").addClass("active");
+  //   $("body").addClass("no-scroll");
+  // });
+
+  // $(".js-modal-close").on("click", function () {
+  //   $("#modal-water").removeClass("active");
+  //   $("body").removeClass("no-scroll");
+  // });
+
+  $('.js-modal-open').on('click', function(e) {
+  e.preventDefault();
+  const target = $(this).data('modal');
+  $('#' + target).addClass('active');
+  $('body').addClass('no-scroll');
+});
+
+$('.js-modal-close').on('click', function() {
+  $(this).closest('.modal').removeClass('active');
+  $('body').removeClass('no-scroll');
+});
+  
+});
